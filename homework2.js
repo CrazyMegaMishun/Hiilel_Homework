fcbbf1cfdbded2f63ca10a9da315ee8d61e2746c
Misha Ohrimenko <misha.ohrimenko2000@gmail.com>
2025-02-10 15:20:23 +0200

    pow() function now supports 0 and below numbers

--- homework2.js
+++ homework2.js
@@ -1,10 +1,52 @@
 function pow(x, y) {
     let result = x
-    for (let i = 0; i<y-1; i++) {
-        result *= x;
-    }
+    switch (true){
+        case x > 0 && y > 0:
+            for (let i = 0; i<y-1; i++){
+                result *= x
+            }
+            return result
 
-    return result
+        case x < 0 && y < 0:
+            for (let i = 0; i < -y; i++) {
+                result *= x
+            }
+            return 1 / result
+
+        case x > 0 && y < 0:
+            for (let i = 0; i < -y; i++) {
+                result *= x
+            }
+            return 1 / result
+
+        case x < 0 && y > 0:
+            for (let i = 0; i < y; i++) {
+                result *= x
+            }
+            return result
+
+        case x === 0:
+            return 0
+        
+        case y === 0:
+            return 1
+    }
 }
 
-console.log(pow(2, 3))
+
+/*
+Возводимое |  Степень
+    +      |     + 
+    -      |     -
+    -      |     +
+    +      |     -
+    0      |   +/-,0    -> 0 при возведении в степень всегда возвращает 0
+  +/-,0    |     0      -> Возведение в нулевую степень всегда возвращает 1
+*/
+
+console.log(pow(2, 3));   // 8
+console.log(pow(2, -3));  // 0.125
+console.log(pow(0, 3));   // 0
+console.log(pow(3, 0));   // 1
+console.log(pow(-2, 3));  // -8
+console.log(pow(-2, -3)); // -0.125
