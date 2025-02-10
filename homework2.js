57583edf932e2f72fedc1f4e690cd4f3d930da99
Misha Ohrimenko <misha.ohrimenko2000@gmail.com>
2025-02-10 18:26:22 +0200

    algoritm optimization

--- homework2.js
+++ homework2.js
@@ -1,29 +1,24 @@
 function pow(x, y) {
     let result = x
     switch (true){
-        case x > 0 && y > 0:
-            for (let i = 0; i<y-1; i++){
-                result *= x
-            }
-            return result
-
-        case (x > 0 && y < 0) || (x < 0 && y < 0):
-            for (let i = 0; i < -y; i++) {
-                result *= x
-            }
-            return 1 / result
-
-        case x < 0 && y > 0:
-            for (let i = 0; i < y; i++) {
-                result *= x
-            }
-            return result
-
         case x === 0:
             return 0
         
         case y === 0:
             return 1
+
+        case y > 0:
+            for (let i = 0; i < y-1; i++){
+                result *= x
+            }
+            return result
+
+        case y < 0:
+            result = 1
+            for (let i = 0; i < -y; i++) {
+                result *= x
+            }
+            return 1 / result
     }
 }
 
