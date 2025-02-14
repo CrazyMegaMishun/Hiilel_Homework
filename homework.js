fe1806a57fce37efab638b1834029cc5ced89f9d
Misha Ohrimenko <misha.ohrimenko2000@gmail.com>
2025-02-14 11:41:30 +0200

    optimizing algo. adding recomendations and .reduce(), .filter() and .map() wor working with data array

--- homework.js
+++ homework.js
@@ -2,44 +2,28 @@ var services = {
     "стрижка": "60 грн",
     "гоління": "80 грн",
     "Миття голови": "100 грн",
+    
+    getPrices: function (){
+        return Object.values(services)
+            .filter(value => typeof value === "string")
+            .map(value => parseInt(value))
+    },
 
     price: function() {
-        let sum = 0
-        Object.values(services).forEach((index) => {
-            if (typeof index === 'string') {
-                sum += Number(index.split(' ')[0])
-            }
-        })
-
-        return sum
+        return this.getPrices().reduce((acc, val) => acc + val)
     },
 
     minPrice: function() {
-        let min = 0
-        Object.values(services).forEach((index) => {
-            if (typeof index === 'string') {
-                if(min === 0) min =  Number(index.split(' ')[0])
-                else min = Math.min(min, Number(index.split(' ')[0]))
-            }
-        })
-        return min
+        return Math.min(...this.getPrices())
     },
 
     maxPrice: function() {
-        let max = 0
-        Object.values(services).forEach((index) => {
-            if (typeof index === 'string') {
-                if(max === 0) max =  Number(index.split(' ')[0])
-                else max = Math.max(max, Number(index.split(' ')[0]))
-            }
-        })
-        return max
-   }
+        return Math.max(...this.getPrices())
+    }
+};
 
-}
-    
-services['Розбити скло'] = "200 грн";
+services["Розбити скло"] = "200 грн"
 
-console.log(services.price())
-console.log(services.minPrice())
+console.log(services.price())  
+console.log(services.minPrice()) 
 console.log(services.maxPrice())
